aa4507243e46921fbc39f0fc9e4cb92ad22d18d8
f <fka@fatihak.in>
2014-04-12 04:13:34 +0300

    structure updated, bourbon added

--- Gemfile
+++ Gemfile
@@ -3,8 +3,9 @@ source 'http://rubygems.org'
 gem "middleman", "~>3.2.2"
 
 gem "middleman-livereload", "~> 3.1.0"
-
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem "middleman-minify-html"
+gem "middleman-deploy"
 
 gem "stringex"
+gem "bourbon"
+gem "rack-google_analytics"
--- config.rb
+++ config.rb
@@ -1,18 +1,20 @@
 require './credentials'
 require 'stringex'
 require 'time'
+require 'rack/google_analytics'
 
 # Custom Config
-DATA_EXT = ".yml"
-API_PREFIX = "/api"
+DATA_EXT = ".yml" # Should be JSON if easier
+API_PREFIX = "/api/v1"
+GOOGLE_ANALYTICS = "UA-0000000-1"
 
-activate :livereload
-
+# Paths
 set :css_dir, 'public/css'
-
 set :js_dir, 'public/js'
-
 set :images_dir, 'public/images'
+
+use Rack::GoogleAnalytics,
+  :web_property_id => GOOGLE_ANALYTICS
 
 Dir["data/*"].each do |path|
     name = File.basename path, DATA_EXT
@@ -26,6 +28,10 @@ helpers do
   end
 end
 
+# Middleman Plugins
+activate :livereload
+activate :minify_html
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
@@ -34,7 +40,7 @@ end
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method   = :rsync
-  deploy.user     = WEBBOX_USER
-  deploy.host     = WEBBOX_HOST
-  deploy.path     = WEBBOX_PATH
+  deploy.user     = DEPLOY_USER
+  deploy.host     = DEPLOY_HOST
+  deploy.path     = DEPLOY_PATH
 end
